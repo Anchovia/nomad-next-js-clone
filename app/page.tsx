608c4e85723ed6d5d27cd406f968a20459a7ae90
Anchovia <hodolye@naver.com>
2024-11-06 17:27:37 +0900

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 export default function Home() {
     return (
         <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5">
-            <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col md:flex-row gap-2">
-                <input
-                    className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
-                    type="text"
-                    placeholder="Search here..."
-                />
-                <button className="bg-black text-white py-2 rounded-full active:scale-90 outline-none transition-transform font-medium md:px-10">
-                    Search
-                </button>
+            <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-4">
+                {["Nico", "Me", "You", "Yourself"].map((person, index) => (
+                    <div
+                        key={index}
+                        className="flex items-center gap-5 odd:bg-gray-100"
+                    >
+                        <div className="size-10 bg-blue-400 rounded-full" />
+                        <span className="text-lg font-medium">{person}</span>
+                        <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
+                            <span>{index}</span>
+                        </div>
+                    </div>
+                ))}
             </div>
         </main>
     );
